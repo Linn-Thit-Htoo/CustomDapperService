1223a62f457f62ffdcd6518bb5b4755dc5c505ca
Linn Thit <lth1212001@gmail.com>
2024-05-26 17:24:05 +0630

    -

--- CustomDapperService.RestApi/Models/UserModel.cs
+++ CustomDapperService.RestApi/Models/UserModel.cs
@@ -1,15 +1,14 @@
-﻿namespace CustomDapperService.RestApi.Models
+﻿namespace CustomDapperService.RestApi.Models;
+
+public class UserModel
 {
-    public class UserModel
-    {
-        public long UserId { get; set; }
-        public string UserName { get; set; } = null!;
-        public string Email { get; set; } = null!;
-        public string? Password { get; set; } = null!;
-        public string UserRole { get; set; } = null!;
-        public int Age { get; set; }
-        public string Gender { get; set; } = null!;
-        public string Address { get; set; } = null!;
-        public bool IsActive { get; set; }
-    }
+    public long UserId { get; set; }
+    public string UserName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string? Password { get; set; } = null!;
+    public string UserRole { get; set; } = null!;
+    public int Age { get; set; }
+    public string Gender { get; set; } = null!;
+    public string Address { get; set; } = null!;
+    public bool IsActive { get; set; }
 }
